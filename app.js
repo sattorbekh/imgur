05d097b475e657a6ee0035ba507825a905a8c1e1
Your Name <sattorbeksadullayev77@gmail.com>
2025-01-06 17:28:15 +0500

    Remove app.js from version control and add it to .gitignore

--- app.js
+++
@@ -1,8 +0,0 @@
-import Upload from './src/upload.js';
-
-(async () => {
-    const image = "/Users/sattorbek/Mac/Documents/mac-wallpaper.jpg";
-    const upload = new Upload();
-    const response = await upload.uploadImage(image);
-    console.log(response);
-})();
